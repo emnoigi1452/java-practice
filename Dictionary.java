702a1c2bc118288ab710f75ba72d31452b4aae1b
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-09-01 10:50:10 +0700

    combine userInterface and dictionary - ex.7
    updated dictionary.java - complete all methods

--- Dictionary.java
+++ Dictionary.java
@@ -1,56 +1,126 @@
-package me.ductrader.exercise;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
-public class Dictionary {
-    public HashMap<String, String> dictionary;
+class Dictionary {
+    public HashMap<String, String> dictionaryDatabase;
+    public ArrayList<String> listWords;
 
     public Dictionary() {
-        dictionary = new HashMap<>();
+        dictionaryDatabase = new HashMap<String, String>();
+        listWords = new ArrayList<String>();
     }
-    public void getTranslation(String param) {
-        if(!dictionary.containsKey(param)) {
-            System.out.println("This word doesn't exist in the dictionary!");
+
+    public int getDictionarySize() {
+        return dictionaryDatabase.size();
+    }
+
+    public void getTranslation(String paramKey) {
+        if(!(dictionaryDatabase.containsKey(paramKey))) {
+            System.out.println("This word doesn't exist in the database!");
         }
         else {
-            dictionary.get(param);
+            System.out.println(paramKey + "- Translation: " + dictionaryDatabase.get(paramKey));
         }
     }
     public void addTranslation(String paramKey, String translation) {
-        if(dictionary.containsKey(paramKey) && dictionary.get(paramKey).equalsIgnoreCase(translation)) {
-            System.out.println("That word has already been translated and stored within the dictionary's database!");
-        }
-        else if(dictionary.containsKey(paramKey) && (!(dictionary.get(paramKey).equalsIgnoreCase(translation)))) {
-            System.out.println("Successfully modified " + paramKey + "! New translation: " + translation);
+        if(dictionaryDatabase.containsKey(paramKey)) {
+            System.out.println("This word already exist in the dictionary!");
         }
         else {
-            dictionary.put(paramKey, translation);
-            System.out.println("Successfully added " + paramKey + " to the database. Translation: " + translation);
+            dictionaryDatabase.put(paramKey, translation);
         }
     }
     public void removeTranslation(String paramKey) {
-        if(!dictionary.containsKey(paramKey)) {
-            System.out.println("This word doesn't exist within the database!");
+        if(!(dictionaryDatabase.containsKey(paramKey))) {
+            System.out.println("This word doesn't exist in the dictionary!");
         }
         else {
-            System.out.println("Word removed!");
-            dictionary.remove(paramKey);
+            dictionaryDatabase.remove(paramKey);
         }
     }
-    public int getWordCount() {
-        return dictionary.size();
-    }
-    public ArrayList<String> translations() {
-        ArrayList<String> words = new ArrayList<>();
-        for(String loadArray: dictionary.keySet()) {
-            words.add(loadArray + " - Translation: " + dictionary.get(loadArray));
+    public void editTranslation(String paramKey, String translation) {
+        if(!(dictionaryDatabase.containsKey(paramKey))) {
+            System.out.println("This word doesn't exist in the dictionary");
         }
-        return words;
+        else {
+            dictionaryDatabase.put(paramKey, translation);
+        }
+    }
+    public ArrayList<String> translation() {
+        for(String word: dictionaryDatabase.keySet()) {
+            listWords.add(word + " - Translation: " + dictionaryDatabase.get(word));
+        }
+        return this.listWords;
     }
     public void printDictionary() {
-        for(String display: translations()) {
-            System.out.println(display);
+        for(String word: listWords) {
+            System.out.println(word);
+        }
+    }
+}
+class UserInterface extends Dictionary {
+    public String cmd;
+    public String[] args;
+    public Scanner sc;
+
+    public UserInterface() {
+        this.sc = new Scanner(System.in);
+    }
+    public void start() {
+        while(true) {
+            System.out.print("Input cmd: ");
+            String cmd = sc.nextLine();
+            args = cmd.split(" ");
+            switch(args[0]) {
+                case "quit":
+                    System.exit(-1);
+                    break;
+                case "translate":
+                    if(args.length != 2) {
+                        System.out.println("Invalid arguments!");
+                    }
+                    else {
+                        super.getTranslation(args[1]);
+                    }
+                case "addTranslation":
+                    if(args.length != 3) {
+                        System.out.println("Invalid arguments!");
+                    }
+                    else {
+                        super.addTranslation(args[1], args[2]);
+                    }
+                case "editTranslation":
+                    if(args.length != 3) {
+                        System.out.println("Invalid arguments!");
+                    }
+                    else {
+                        super.editTranslation(args[1], args[2]);
+                    }
+                case "remove":
+                    if(args.length != 2) {
+                        System.out.println("Invalid arguments!");
+                    }
+                    else {
+                        super.removeTranslation(args[1]);
+                    }
+                case "print":
+                    if(args.length != 1) {
+                        System.out.println("Invalid arguments!");
+                    }
+                    else {
+                        super.printDictionary();
+                    }
+                case "getSize":
+                    if(args.length != 1) {
+                        System.out.println("Invalid arguments!");
+                    }
+                    else {
+                        System.out.println("Dictionary size: " + super.getDictionarySize());
+                    }
+                default:
+                    System.out.println("Unknown command!");
+            }
         }
     }
 }
