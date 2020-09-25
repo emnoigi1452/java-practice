ab6c19fba92ea799fdeace6d171faf75b44a861f
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-09-25 12:22:05 +0700

    update array length handling

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,24 +1,30 @@
-package me.ductrader.javapractice;
-
 public class Main {
     public static int secondLargest(int[] array) {
-        int largest = array[0];
-        int second = array[0];
-        for(int i = 0; i < array.length; i++) {
-            if(array[i] > largest) {
-                largest = array[i];
+        if(array.length == 0) {
+            System.out.println("Array is empty!");
+            return -1;
+        } else if(array.length == 1) {
+            System.out.println("Array only has one element, returning...");
+            return array[0];
+        } else {
+            int largest = array[0];
+            int second = array[0];
+            for(int i = 0; i < array.length; i++) {
+                if(array[i] > largest) {
+                    largest = array[i];
+                }
             }
-        }
-        for(int i = 0; i < array.length; i++) {
-            if(array[i] > second && array[i] < largest) {
-                second = array[i];
+            for(int i = 0; i < array.length; i++) {
+                if(array[i] > second && array[i] < largest) {
+                    second = array[i];
+                }
             }
+            return second;
         }
-        return second;
     }
 
     public static void main(String[] args) {
-        int[] ar = {0,1,2,3,4};
+        int[] ar = {};
         System.out.println(secondLargest(ar));
     }
 }
