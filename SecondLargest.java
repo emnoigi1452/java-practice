8f1182c31544ec820c8651731319bea0321f893c
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-09-25 12:26:20 +0700

    update algorithm

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,3 +1,5 @@
+package me.ductrader.javapractice;
+
 public class Main {
     public static int secondLargest(int[] array) {
         if(array.length == 0) {
@@ -7,8 +9,8 @@ public class Main {
             System.out.println("Array only has one element, returning...");
             return array[0];
         } else {
-            int largest = array[0];
-            int second = array[0];
+            int largest = 0;
+            int second = 0;
             for(int i = 0; i < array.length; i++) {
                 if(array[i] > largest) {
                     largest = array[i];
@@ -24,7 +26,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] ar = {};
+        int[] ar = {0,1,2,3,4};
         System.out.println(secondLargest(ar));
     }
 }
