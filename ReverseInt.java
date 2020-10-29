69c5428ca6e027590e57f45ea07e9e9942409a50
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-10-29 18:06:25 +0700

    leetcode - runtime :1ms - memory: 36mb

--- ReverseInt.java
+++ ReverseInt.java
@@ -1,28 +1,19 @@
-import java.util.Scanner;
-
-class Main {
-    static int reverse(int given) {
-        String givenAsString = Integer.toString(given);
-        String reverse = "";
-        if(given < 0) {
-            givenAsString = givenAsString.replace("-", "");
+class Solution {
+    public int reverse(int x) {
+        int d = 0; long y = 0;
+        while(Math.abs(x) > 0) {
+            d = x % 10;
+            x /= 10;
+            y = (y * 10) + (long)d;
         }
-        for(int i = givenAsString.length() - 1; i >= 0; i--) {
-            reverse += givenAsString.charAt(i);
+        if(y > (int)Math.pow(2, 31) || y < (int)Math.pow(2,31)*(-1)) {
+            return 0;
+        } else {
+            if(x < 0) {
+                return (int)y * (-1);
+            } else {
+                return (int)y;
+            }
         }
-
-        if(given < 0) {
-            return Integer.parseInt(reverse) * (-1);
-        }
-        else {
-            return Integer.parseInt(reverse);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input: ");
-        int r = Integer.parseInt(sc.nextLine());
-        System.out.println("Output: " + reverse(r));
     }
 }
