3d6ec727ec69459a99cd6c01696615c467ca9736
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-11-21 21:01:28 +0700

    Calculate the average score of a subject
    Input will require you to input 3 lines : (Note that score n must satisfy 0 <= n <= 10)
    +) Line 1: Consists of double/float values that are your scores with a multiplier of 1 (15' exams, revisions,..etc..)
    +) Line 2: Consists of double/float values that are your scores with a multiplier of 2 (45'/90' exams, at max there could only be 10 scores)
    +) Line 3: Consists your finals score (With a multiplier of 3)

---
+++ AverageScore.java
@@ -0,0 +1,88 @@
+package me.ductrader.javapractice;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static double average(double[] hs1, double[] hs2, double[] hs3) {
+        double totalValue = 0;
+        for(Double i: hs1) {
+            totalValue += i;
+        }
+        for(Double j: hs2) {
+            totalValue += j*2;
+        }
+        for(Double y: hs3) {
+            totalValue += y*3;
+        }
+        return totalValue / (hs1.length + (hs2.length * 2) + (hs3.length * 3));
+    }
+
+    public static boolean isNumeric(String param) {
+        try {
+            Double.parseDouble(param);
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        File f1 = new File("diem.in");
+        File f2 = new File("diem.out");
+        if(!(f1.exists())) {
+            System.out.println("Cannot locate input file, generating a new one...");
+            try {
+                f1.createNewFile();
+                System.out.println("File created!");
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(!(f2.exists())) {
+            System.out.println("Cannot locate output file, generating a new output file...");
+            try {
+                f2.createNewFile();
+                System.out.println("File created!");
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            Scanner sc = new Scanner(f1);
+            PrintWriter wr = new PrintWriter(f2);
+            double[] h1 = new double[20]; double[] h2 = new double[10]; double[] h3 = new double[2];
+            main:
+            for(int k = 0; k < 3; k++) {
+                if(sc.hasNextLine()) {
+                    String h = sc.nextLine();
+                    double[] scores = new double[h.split(" ").length]; int index = 0;
+                    for(int z = 0; z < h.split(" ").length; z++) {
+                        if(isNumeric(h.split(" ")[z]) && Double.parseDouble(h.split(" ")[z]) <= 10 &&
+                                Double.parseDouble(h.split(" ")[z]) >= 0) {
+                            scores[index] = Double.parseDouble(h.split(" ")[z]); index++;
+                        } else {
+                            wr.write("Invalid scores!"); wr.flush(); wr.close(); break main;
+                        }
+                    }
+                    switch (k) {
+                        case 0 -> h1 = scores;
+                        case 1 -> h2 = scores;
+                        case 2 -> h3 = scores;
+                    }
+                } else {
+                    wr.println("Not enough arguments created!");
+                    wr.flush(); wr.close();
+                    break;
+                }
+            }
+            double avg = average(h1, h2, h3);
+            wr.print("Average score: "); wr.printf("%.1f", avg);
+            wr.flush(); wr.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
